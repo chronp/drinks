5c4da76e8ee4a5379eeae59609bfffe4aa7459b4
chronp <chroniak.p@gmail.com>
2021-01-14 21:24:42 -0500

    asd

--- tiki.ps1
+++ tiki.ps1
@@ -1,33 +1,55 @@
-Add-Type -assembly System.Windows.Forms
-$main_form = New-Object System.Windows.Forms.Form
-$main_form.Text ='Tiki Drinks'
-$main_form.Width = 600
-$main_form.Height = 400
-$main_form.AutoSize = $true
-$main_form.ShowDialog()
-
-$Label = New-Object System.Windows.Forms.Label
-
-$Label.Text = "AD users"
-
-$Label.Location  = New-Object System.Drawing.Point(0,10)
-
-$Label.AutoSize = $true
-
-$main_form.Controls.Add($Label)
-
-
 $rum_index = Import-Csv -Path .\rum_index.csv
 $drinks = Import-Csv -Path .\tiki.csv
 
-$puka = $drinks -match 'puka'
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Select a Computer'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(75,120)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(150,120)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please select a computer:'
+$form.Controls.Add($label)
+
+$listBox = New-Object System.Windows.Forms.ListBox
+$listBox.Location = New-Object System.Drawing.Point(10,40)
+$listBox.Size = New-Object System.Drawing.Size(260,20)
+$listBox.Height = 80
+
+$cocktails=($drinks.cocktail | Get-Unique)
+Foreach ($cocktail in $cocktails){
+
+[void] $listBox.Items.Add($cocktail)
+
+}
 
 
- $ingredient.Cocktail
+$form.Controls.Add($listBox)
+$form.Topmost = $true
+$result = $form.ShowDialog()
 
-foreach ($ingredient in $puka){
- $ingredient.ingredient +" "+ $ingredient.amount+" " + $ingredient.units
-
- }
-
- 
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $listBox.SelectedItem
+    $x
+}
