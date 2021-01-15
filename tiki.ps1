2bf7d1ec6a018d45a55821043de906674bb904ec
chronp <chroniak.p@gmail.com>
2021-01-14 20:12:48 -0500

    testing

--- tiki.ps1
+++ tiki.ps1
@@ -1,5 +1,24 @@
+Add-Type -assembly System.Windows.Forms
+$main_form = New-Object System.Windows.Forms.Form
+$main_form.Text ='Tiki Drinks'
+$main_form.Width = 600
+$main_form.Height = 400
+$main_form.AutoSize = $true
+$main_form.ShowDialog()
+
+$Label = New-Object System.Windows.Forms.Label
+
+$Label.Text = "AD users"
+
+$Label.Location  = New-Object System.Drawing.Point(0,10)
+
+$Label.AutoSize = $true
+
+$main_form.Controls.Add($Label)
+
+
 $rum_index = Import-Csv -Path .\rum_index.csv
-$drinks = Import-Csv -Path .\tkik.csv
+$drinks = Import-Csv -Path .\tiki.csv
 
 $puka = $drinks -match 'puka'
 
@@ -11,4 +30,4 @@ foreach ($ingredient in $puka){
 
  }
 
-#$drinks -match 'puka' | $drinks -match 'rum' 
+ 
